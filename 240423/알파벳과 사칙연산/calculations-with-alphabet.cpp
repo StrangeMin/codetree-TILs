a9cb7c7593b432e99d0ff5394fe0feb38620e5ee
StrangeMin <116520932+StrangeMin@users.noreply.github.com>
2024-04-23 16:41:28 +0900

    Title: 알파벳과 사칙연산, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240423/알파벳과 사칙연산/calculations-with-alphabet.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+int N, M, C, ret;
+int board[11][11];
+string str;
+int alpha[26];
+
+int calculate(int sum, char op, int num) {
+	if (op == '+')
+		return sum + num;
+	else if (op == '-')
+		return sum - num;
+	else if (op == '*')
+		return sum * num;
+
+}
+
+void go(int index, int sum) {
+	if (index == str.size() - 1) {
+		ret = max(ret, sum);
+		return;
+	}
+
+	if (alpha[str[index + 2] - 'a']) {
+		go(index + 2, calculate(sum, str[index + 1], alpha[str[index + 2] - 'a']));
+	}
+	else {
+		for (int i = 1; i <= 4; i++) {
+			alpha[str[index + 2]] = i;
+			go(index + 2, calculate(sum, str[index + 1], i));
+		}
+	}
+	
+}
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> str;
+
+	for (int i = 1; i <= 4; i++) {
+		alpha[str[0] - 'a'] = i;
+		go(0, i);
+	}
+
+	cout << ret << "\n";
+}
