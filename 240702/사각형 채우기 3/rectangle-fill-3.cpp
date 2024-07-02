449e8714e6bc208226c9580f8e4c1688f6feb6a6
StrangeMin <116520932+StrangeMin@users.noreply.github.com>
2024-07-02 22:27:03 +0900

    Title: 사각형 채우기 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240702/사각형 채우기 3/rectangle-fill-3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int dp[1001];
+
+
+int main() {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    dp[1] = 2;
+    dp[2] = 7;
+    dp[3] = 22;
+
+    for(int i=4; i<=N; i++){
+        dp[i] = ((dp[i-1] * 3 + dp[i-2] - dp[i-3]))%1000000007;
+    }
+
+    cout << dp[N] << "\n";
+
+    return 0;
+}
