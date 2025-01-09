fcc502d2ffe19bbd6d12fe65324bb2ff64219119
StrangeMin <116520932+StrangeMin@users.noreply.github.com>
2025-01-09 20:22:04 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 13ms, Memory: 0MB, Status: Passed - CodeTree

--- 250109/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.cpp
+++ 250109/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.cpp
@@ -1,62 +1,82 @@
 #include <iostream>
 #include <algorithm>
-#include <cmath>
+#include <climits>
+
+#define MAX_N 100
+#define MAX_R 100
 
 using namespace std;
 
+// 변수 선언
 int n;
-int grid[100][100];
-pair<int,int> ret[100][100];
+int num[MAX_N][MAX_N];
+int dp[MAX_N][MAX_N];
+
+int ans = INT_MAX;
+
+void Initialize() {
+    // 전부 INT_MAX로 초기화합니다.
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            dp[i][j] = INT_MAX;
+
+    // 시작점의 경우 dp[0][0] = num[0][0]으로 초기값을 설정해줍니다
+    dp[0][0] = num[0][0];
+
+    // 최좌측 열의 초기값을 설정해줍니다.
+    for(int i = 1; i < n; i++)
+        dp[i][0] = max(dp[i - 1][0], num[i][0]);
+
+    // 최상단 행의 초기값을 설정해줍니다.
+    for(int j = 1; j < n; j++)
+        dp[0][j] = max(dp[0][j - 1], num[0][j]);
+}
+
+int Solve(int lower_bound) {
+    // lower_bound 미만의 값은 사용할 수 없도록
+    // num값을 변경해줍니다.
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            if(num[i][j] < lower_bound)
+                num[i][j] = INT_MAX;
+    
+    // DP 초기값 설정
+    Initialize();
+
+    // 탐색하는 위치의 위에 값과 좌측 값 중에 작은 값과
+    // 해당 위치의 숫자 중에 최댓값을 구해줍니다.
+    for(int i = 1; i < n; i++)
+        for(int j = 1; j < n; j++)
+            dp[i][j] = max(min(dp[i - 1][j], dp[i][j - 1]), num[i][j]);
+        
+    return dp[n - 1][n - 1];
+}
 
 int main() {
+    // 입력
     cin >> n;
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cin >> grid[i][j];
-        }
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            cin >> num[i][j];
+
+    // 최솟값을 lower_bound라고 가정했을 때
+    // lower_bound 이상의 수들만 사용하여 
+    // 이동한다는 조건하에서
+    // 최댓값을 최소로 만드는 DP 문제를 풀어줍니다.
+    for(int lower_bound = 1; lower_bound <= MAX_R; lower_bound++) {
+        int upper_bound = Solve(lower_bound);
+        
+        // 다 진행했음에도 여전히 INT_MAX라면 
+        // 그러한 이동이 불가능하다는 뜻이므로
+        // 패스합니다.
+        if(upper_bound == INT_MAX)
+            continue;
+        
+        // 답을 갱신합니다.
+        ans = min(ans, upper_bound - lower_bound);
     }
 
-    ret[0][0] = {grid[0][0], grid[0][0]};
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-
-            if(i==0 && j==0)
-                continue;
-
-            // 위
-            int ny1 = i -1;
-            int nx1 = j;
-            int sum1 = 999999999;
-
-            if(ny1 >= 0){
-                sum1 = abs(max(grid[i][j], ret[ny1][nx1].second) -  min(grid[i][j], ret[ny1][nx1].first));
-            }
-
-            // 왼쪽    
-            int ny2 = i;
-            int nx2 = j-1;
-            int sum2 = 999999999;
-
-            if(nx2 >= 0){
-                sum2 = abs(max(grid[i][j], ret[ny2][nx2].second) -  min(grid[i][j], ret[ny2][nx2].first));
-            }
-
-            if(sum1 < sum2){
-                ret[i][j] = {min(grid[i][j], ret[ny1][nx1].first), max(grid[i][j], ret[ny1][nx1].second)};
-            }
-            else {
-                ret[i][j] = {min(grid[i][j], ret[ny2][nx2].first), max(grid[i][j], ret[ny2][nx2].second)};
-            }
-        }
-    }
-
-
-    cout << ret[n-1][n-1].second - ret[n-1][n-1].first << "\n";
-    
-
-    // Write your code here!
-
+    cout << ans;
     return 0;
 }
