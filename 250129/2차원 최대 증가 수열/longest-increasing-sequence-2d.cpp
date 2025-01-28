0117afe981b38e75ac79d2b570e4d074620637ae
StrangeMin <116520932+StrangeMin@users.noreply.github.com>
2025-01-29 03:04:52 +0900

    Title: 2차원 최대 증가 수열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250129/2차원 최대 증가 수열/longest-increasing-sequence-2d.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, m;
+int grid[50][50];
+int dp[50][50];
+int ret;
+
+void getJumpCnt(int y, int x){
+
+    for(int i=0; i<=y-1; i++){
+        for(int j=0; j<=x-1; j++){
+            if(grid[y][x] > grid[i][j])
+                dp[y][x] = max(dp[y][x], dp[i][j] + 1);
+
+            ret = max(ret, dp[y][x]);
+        }
+    }
+}
+
+void printArr(){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout << dp[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    for(int i=0; i<m; i++)
+        dp[0][i] = 1;
+
+    for(int i=1; i<n; i++){
+        for(int j=1; j<n; j++){
+            getJumpCnt(i,j);
+        }
+    }
+
+    //printArr();
+    // Write your code here!
+    cout << ret << "\n";
+    
+    return 0;
+}
