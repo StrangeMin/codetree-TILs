cdb107aed709c6b249ee41070fb41886c3d5307a
StrangeMin <116520932+StrangeMin@users.noreply.github.com>
2025-07-20 02:27:54 +0900

    Title: 두 수의 합, Time: 24ms, Memory: 2MB, Status: Passed - Codetree

--- 250720/두 수의 합/sum-of-two-num.cpp
+++ 250720/두 수의 합/sum-of-two-num.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
-#include <map>
+#include <unordered_map>
 using namespace std;
 
-int n, k;
-int arr[100000];
-map<long long,long long> m;
+long long n, k;
+long long arr[100000];
+unordered_map<long long,long long> m;
 long long ret;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     cin >> n >> k;
     
     for (int i = 0; i < n; i++) {
@@ -20,8 +22,9 @@ int main() {
 
         if(tmp == it.first){
             ret += (it.second *(it.second-1))/2;
+            m[tmp] = 0;
         }
-        else if(m[tmp] != 0){
+        else if(m.find(tmp) != m.end()){
             ret += it.second * m[tmp];
             m[tmp] = 0;
         }
