d776e59506f0ec430979ea77e3ed1334ab52c6a2
StrangeMin <116520932+StrangeMin@users.noreply.github.com>
2025-07-20 04:11:46 +0900

    Title: 특별한 문자, Time: 10ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250720/특별한 문자/special-character.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string str;
+pair<int,int> arr[26];
+
+int findIdx(){
+    int idx = 100;
+    int retIdx = 100;
+    for(int i=0; i<26; i++){
+        if(arr[i].first == 1 && arr[i].second < idx){
+            idx = arr[i].second;
+            retIdx = i;
+        }
+    }
+
+    return retIdx;
+}
+
+int main() {
+    cin >> str;
+
+    for(int i=0; i<str.size(); i++){
+        arr[str[i]-97] = {arr[str[i]-97].first + 1, i};
+    }
+
+    int idx = findIdx();
+
+    if(idx == 100)
+        cout << "None" << "\n";
+    else
+        cout << (char)(idx+97) << "\n";
+
+    return 0;
+}
