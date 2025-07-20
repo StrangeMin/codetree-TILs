1e47b2cfc4eac1270030cfdc14463d9216184d3b
StrangeMin <116520932+StrangeMin@users.noreply.github.com>
2025-07-20 18:17:15 +0900

    Title: 서로 다른 수의 개수, Time: 34ms, Memory: 2MB, Status: Passed - Codetree

---
+++ 250720/서로 다른 수의 개수/distinct-numbers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+const int MAX_N = 100000;
+int n;
+int arr[MAX_N];
+unordered_set<int> s;
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        s.insert(arr[i]);
+    }
+
+    cout << s.size() << "\n";
+
+    return 0;
+}
